0f4e3f5de8cba27be42a6f01db10e106c66fd89c
shivs2000 <singhshivam0666@gmail.com>
2022-08-24 21:45:57 +0530

    added tailwind, modified _app.tsx file

--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -17,5 +17,9 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    post,
+    comment,
+    postedBy,
+    user,
   ]),
 });
