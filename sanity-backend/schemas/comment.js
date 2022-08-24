c91a24e7193b13f3946a9088d67516ae6305b7f1
shivs2000 <singhshivam0666@gmail.com>
2022-08-24 12:27:55 +0530

    added schemas and dependencies

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBY",
+      title: "Posted By",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
